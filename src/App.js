af1032655d565b288b3a0d8a4c430ce9504e913d
Cuitlazotlac <cuitlazotlac@gmail.com>
2021-04-02 11:56:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Typical from "react-typical";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1>Hi it's Cuitlazotlac</h1>
+        <p> I'm a{' '}
+          <Typical
+            loop={Infinity}
+            wrapper="b"
+            steps={[
+              "Product Manager 👨🏻‍💻",
+              5000,
+              "Web Designer 🦕",
+              5000,
+              "Data-driven Guy 📈",
+              5000,
+              "French 🇫🇷 Egyptian 🇪🇬 Mexican 🇲🇽 Guy",
+              5000,
+            ]}
+          />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
